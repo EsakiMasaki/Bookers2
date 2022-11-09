d81b88943c1a30bfb82d42069b50c796455ce4f1
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2022-11-09 08:18:36 +0000

    [Update]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
   def index
+    @book = current_user.books.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.user_id)
   end
 
   def show
+    @book = current_user.books.new
   end
 
   def edit
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
