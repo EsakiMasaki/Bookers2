edd6380ff5c84c4bd0cd8b5792d03ab58ebdbea7
EsakiMasaki <115428948+EsakiMasaki@users.noreply.github.com>
2022-11-11 06:55:48 +0000

    [Finish]4-27

--- db/migrate/20221108065554_devise_create_users.rb
+++ db/migrate/20221108065554_devise_create_users.rb
@@ -37,7 +37,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       t.timestamps null: false
     end
     
-    add_index :users, :name, length: {minimum: 2,maximum: 20}
+    add_index :users, :name, length: {minimum: 2,maximum: 20}, unique: true
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
